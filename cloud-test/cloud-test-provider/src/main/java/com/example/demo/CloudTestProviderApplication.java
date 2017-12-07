e7c90683bfab59a6f54f09d66c788c5ff3462ae1
刘成 <790809568@qq.com>
2017-12-07 16:26:32 +0800

    first commit

--- cloud-test/cloud-test-provider/src/main/java/com/example/demo/CloudTestProviderApplication.java
+++ cloud-test/cloud-test-provider/src/main/java/com/example/demo/CloudTestProviderApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
 @EnableFeignClients//启动Feign
 @EnableHystrix//开启断路器功能
+@ComponentScan({"com.example.demo","spring.cloud.api"})
 public class CloudTestProviderApplication {
 
 	public static void main(String[] args) {
--- zuul/src/main/java/spring/cloud/zuul/ZuulApplication.java
+++ zuul/src/main/java/spring/cloud/zuul/ZuulApplication.java
@@ -2,8 +2,10 @@ package spring.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
 
 	public static void main(String[] args) {
